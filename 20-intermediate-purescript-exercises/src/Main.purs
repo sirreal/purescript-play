e256b8b8fcbe8b3c432eb01a5e1999c29b66d6db
Jon Surrell <jon.surrell@gmail.com>
2017-02-12 22:54:47 +0100

    remove solutions

--- 20-intermediate-purescript-exercises/src/Main.purs
+++ 20-intermediate-purescript-exercises/src/Main.purs
@@ -15,19 +15,17 @@ class Fluffy f where
 -- Exercise 1
 -- Relative Difficulty: 1
 instance fluffyList :: Fluffy List where
-  furry _ Nil         = Nil
-  furry f (Cons x xs) = Cons (f x) (furry f xs)
+  furry = unsafeCoerce "todo"
 
 -- Exercise 2
 -- Relative Difficulty: 1
 instance fluffyMaybe :: Fluffy Maybe where
-  furry _ Nothing = Nothing
-  furry f (Just x) = Just (f x)
+  furry = unsafeCoerce "todo"
 
 -- Exercise 3
 -- Relative Difficulty: 5
 instance fluffyFunction :: Fluffy ((->) t) where
-  furry = (<<<)
+  furry = unsafeCoerce "todo"
 
 newtype EitherLeft b a = EitherLeft (Either a b)
 newtype EitherRight a b = EitherRight (Either a b)
@@ -35,14 +33,12 @@ newtype EitherRight a b = EitherRight (Either a b)
 -- Exercise 4
 -- Relative Difficulty: 5
 instance fluffyEitherLeft :: Fluffy (EitherLeft t) where
-  furry _ (EitherLeft (Right x)) = EitherLeft (Right x)
-  furry f (EitherLeft (Left x)) = EitherLeft (Left (f x))
+  furry = unsafeCoerce "todo"
 
 -- Exercise 5
 -- Relative Difficulty: 5
 instance fluffyEitherRight :: Fluffy (EitherRight t) where
-  furry _ (EitherRight (Left x)) = EitherRight (Left x)
-  furry f (EitherRight (Right x)) = EitherRight (Right (f x))
+  furry = unsafeCoerce "todo"
 
 class Misty m where
   banana :: forall a b. (a -> m b) -> m a -> m b
@@ -53,86 +49,81 @@ class Misty m where
   furry' :: forall a b. (a -> b) -> m a -> m b
 
 defaultMistyFurry' :: forall a b m. Misty m => (a -> b) -> m a -> m b
-defaultMistyFurry' f = banana (unicorn <<< f)
+defaultMistyFurry' = unsafeCoerce "todo"
 
 -- Exercise 7
 -- Relative Difficulty: 2
 instance mistyList :: Misty List where
-  banana = concatMap
-  unicorn x = Cons x Nil
+  banana = unsafeCoerce "todo"
+  unicorn = unsafeCoerce "todo"
   furry' f ma = defaultMistyFurry' f ma
 
 -- Exercise 8
 -- Relative Difficulty: 2
 instance mistyMaybe :: Misty Maybe where
-  banana _ Nothing  = Nothing
-  banana f (Just x) = f x
-  unicorn = Just
+  banana = unsafeCoerce "todo"
+  unicorn = unsafeCoerce "todo"
   furry' f ma = defaultMistyFurry' f ma
 
 -- Exercise 9
 -- Relative Difficulty: 6
 instance mistyFunc :: Misty ((->) t) where
   banana = unsafeCoerce "todo"
-  unicorn = const
+  unicorn = unsafeCoerce "todo"
   furry' f ma = defaultMistyFurry' f ma
 
 -- Exercise 10
 -- Relative Difficulty: 6
 instance mistyEitherLeft :: Misty (EitherLeft t) where
-  banana f (EitherLeft (Left x)) = f x
-  banana _ (EitherLeft (Right x)) = EitherLeft (Right x)
-  unicorn = EitherLeft <<< Left
+  banana = unsafeCoerce "todo"
+  unicorn = unsafeCoerce "todo"
   furry' f ma = defaultMistyFurry' f ma
 
 -- Exercise 11
 -- Relative Difficulty: 6
 instance mistyEitherRight :: Misty (EitherRight t) where
-  banana f (EitherRight (Right x)) = f x
-  banana _ (EitherRight (Left x)) = EitherRight (Left x)
-  unicorn = EitherRight <<< Right
+  banana = unsafeCoerce "todo"
+  unicorn = unsafeCoerce "todo"
   furry' f ma = defaultMistyFurry' f ma
 
 -- Exercise 12
 -- Relative Difficulty: 3
 jellybean :: forall a m. Misty m => m (m a) -> m a
-jellybean = banana id
+jellybean = unsafeCoerce "todo"
 
 -- Exercise 13
 -- Relative Difficulty: 6
 apple :: forall a b m. Misty m => m a -> m (a -> b) -> m b
-apple ma mfab = banana ((flip furry') ma) mfab
+apple = unsafeCoerce "todo"
 
 -- Exercise 14
 -- Relative Difficulty: 6
 moppy :: forall a b m. Misty m => List a -> (a -> m b) -> m (List b)
-moppy xs f = foldr f' (unicorn Nil) xs
-  where
-    f' el acc = apple acc (furry' Cons (f el))
+moppy = unsafeCoerce "todo"
 
 -- Exercise 15
 -- Relative Difficulty: 6
 -- (bonus: use moppy)
 sausage :: forall a m. Misty m => List (m a) -> m (List a)
-sausage = (flip moppy) (furry' id)
+sausage = unsafeCoerce "todo"
 
 -- Exercise 16
 -- Relative Difficulty: 6
 -- (bonus: use apple + furry')
 banana2 :: forall a b c m. Misty m => (a -> b -> c) -> m a -> m b -> m c
-banana2 f ma mb = apple mb (furry' f ma)
+banana2 = unsafeCoerce "todo"
 
 -- Exercise 17
 -- Relative Difficulty: 6
 -- (bonus: use apple + banana2)
 banana3 :: forall a b c d m. Misty m => (a -> b -> c -> d) -> m a -> m b -> m c -> m d
-banana3 f ma mb mc = apple mc (banana2 f ma mb)
+banana3 = unsafeCoerce "todo"
 
 -- Exercise 18
 -- Relative Difficulty: 6
 -- (bonus: use apple + banana3)
 banana4 :: forall a b c d e m. Misty m => (a -> b -> c -> d -> e) -> m a -> m b -> m c -> m d -> m e
-banana4 f ma mb mc md = apple md (banana3 f ma mb mc)
+banana4 = unsafeCoerce "todo"
 
 newtype State s a = State (s -> Tuple s a)
 
@@ -143,18 +134,11 @@ instance newtypeState :: Newtype (State s a) (s -> Tuple s a) where
 -- Exercise 19
 -- Relative Difficulty: 9
 instance fluffyState :: Fluffy (State s) where
-  furry f state = State (\s -> let prev = (unwrap state) s
-                               in  Tuple (fst prev) (f (snd prev))
-                  )
+  furry = unsafeCoerce "todo"
 
 -- Exercise 20
 -- Relative Difficulty: 10
 instance mistyState :: Misty (State s) where
-  banana f s = State (
-    \x -> let prev = unwrap s x
-              y = fst prev
-              a = snd prev
-          in unwrap (f a) y
-  )
-  unicorn x = State (\s -> Tuple s x)
+  banana = unsafeCoerce "todo"
+  unicorn = unsafeCoerce "todo"
   furry' f ma = defaultMistyFurry' f ma
